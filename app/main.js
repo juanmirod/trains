b5891fe39cb2d55e58a0789019bc9a3a339615aa
Juanmi Rodriguez Cerón <juanmi.rodriguez.ceron@gmail.com>
2016-08-28 20:45:45 +0200

    refactor: Moved inputs population to a function
    Now we get sure that the inputs get populated after the
    document have been loaded.

--- app/main.js
+++ app/main.js
@@ -1,7 +1,44 @@
 (function() {
+
+  function ready() {
+      
+    var promise = new Promise(function(resolve, reject) {
+      document.addEventListener('readystatechange', function() {
+        if(document.readyState !== 'loading') {
+          resolve();
+        }
+      });
+    });
+
+    return promise;
+
+  };
+
+  function loadStops() {
+
+    var stops = Routes.stops();
+    
+    stops.then(function(result){
+      //console.log(result);
+      var departures = document.getElementById('departure-stops');
+      var arrivals = document.getElementById('arrival-stops');
+
+      result.forEach( function addStop(stop) {
+        
+        var option = '<option value="' + stop.stop_name + ' - ' + stop.stop_id + '"></option>';
+        departures.innerHTML += option;
+        arrivals.innerHTML += option;
+
+      });
+
+    });
+
+  };
+
+  ready().then(loadStops);
   
   // TODO: get all the lines 
-  var routes = Routes.routes();
+  /*var routes = Routes.routes();
   routes.then(function(result){
     console.log(result);
   });
@@ -9,28 +46,6 @@
   var trips = Routes.trips();
   trips.then(function(result){
     console.log(result);
-  });
-
-  var stops = Routes.stops();
-  stops.then(function(result){
-    //console.log(result);
-    var departures = document.getElementById('departure-stops');
-    var arrivals = document.getElementById('arrival-stops');
-
-    result.forEach( function(stop) {
-      // statements
-      var option = '<option value="' + stop.stop_name + ' - ' + stop.stop_id + '"></option>';
-      departures.innerHTML += option;
-      arrivals.innerHTML += option;    
-    });
-
-  });
-
-
-  // TODO: get all stops for each line
-
-  // TODO: populate the input fields
-
-  // TODO: add a submit handler
+  });*/
 
 })();
--- dist/js/main.js
+++ dist/js/main.js
@@ -2,34 +2,46 @@
 
 (function () {
 
-  // TODO: get all the lines 
-  var routes = Routes.routes();
-  routes.then(function (result) {
-    console.log(result);
-  });
+  function ready() {
 
-  var trips = Routes.trips();
-  trips.then(function (result) {
-    console.log(result);
-  });
-
-  var stops = Routes.stops();
-  stops.then(function (result) {
-    //console.log(result);
-    var departures = document.getElementById('departure-stops');
-    var arrivals = document.getElementById('arrival-stops');
-
-    result.forEach(function (stop) {
-      // statements
-      var option = '<option value="' + stop.stop_name + ' - ' + stop.stop_id + '"></option>';
-      departures.innerHTML += option;
-      arrivals.innerHTML += option;
+    var promise = new Promise(function (resolve, reject) {
+      document.addEventListener('readystatechange', function () {
+        if (document.readyState !== 'loading') {
+          resolve();
+        }
+      });
     });
+
+    return promise;
+  };
+
+  function loadStops() {
+
+    var stops = Routes.stops();
+
+    stops.then(function (result) {
+      //console.log(result);
+      var departures = document.getElementById('departure-stops');
+      var arrivals = document.getElementById('arrival-stops');
+
+      result.forEach(function addStop(stop) {
+
+        var option = '<option value="' + stop.stop_name + ' - ' + stop.stop_id + '"></option>';
+        departures.innerHTML += option;
+        arrivals.innerHTML += option;
+      });
+    });
+  };
+
+  ready().then(loadStops);
+
+  // TODO: get all the lines 
+  /*var routes = Routes.routes();
+  routes.then(function(result){
+    console.log(result);
   });
-
-  // TODO: get all stops for each line
-
-  // TODO: populate the input fields
-
-  // TODO: add a submit handler
+   var trips = Routes.trips();
+  trips.then(function(result){
+    console.log(result);
+  });*/
 })();
